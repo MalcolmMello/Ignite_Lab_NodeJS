5391f7e3a98dfad7dfff3fd34d43f28a8daf1721
malcolm <malcolmmellolima@gmail.com>
2022-12-17 16:01:35 -0300

    persistência de dados e rotas http

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from "@prisma/client";
 import { Notification } from "@application/entities/notification";
+import { Content } from "@application/entities/content";
 
 export class PrismaNotificationMapper {
     static toPrisma(notification: Notification) {
@@ -9,5 +11,16 @@ export class PrismaNotificationMapper {
             recipientId: notification.recipientId,
             readAt: notification.readAt
         }
+    }
+
+    static toDomain(raw: RawNotification): Notification {
+        return new Notification({
+            category: raw.category,
+            content: new Content(raw.content),
+            recipientId: raw.recipientId,
+            readAt: raw.readAt,
+            canceledAt: raw.canceledAt,
+            createdAt: raw.createdAt
+        }, raw.id);
     }
 }
